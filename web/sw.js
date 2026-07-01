42e3a114308b1a73a2d5055902519d8fb46984c5
bherrero25 <betsabe.herrero@gmail.com>
2026-07-01 13:23:32 +0200

    feat: service worker con auto-actualización de caché PWA

---
+++ web/sw.js
@@ -0,0 +1,26 @@
+const VERSION = 'v3';
+const CACHE = 'sportmatch-' + VERSION;
+const ASSETS = ['/', '/app.html', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).then(res => {
+      const clone = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return res;
+    }).catch(() => caches.match(e.request))
+  );
+});
