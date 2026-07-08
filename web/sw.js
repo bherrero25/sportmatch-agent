08d6b1982e19adb4f1e7b67dacf9544f6723a3ad
bherrero25 <betsabe.herrero@gmail.com>
2026-07-08 17:00:59 +0200

    fix: botón enviar del chat visible en móvil (sin zoom iOS, input encogible, safe area)

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v17';
+const VERSION = 'v18';
 const CACHE = 'sportmatch-' + VERSION;
 const ASSETS = ['/', '/app.html', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 
