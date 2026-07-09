ce56a242bb94666f52b0bcaf070dbb0a5e386d58
bherrero25 <betsabe.herrero@gmail.com>
2026-07-09 12:38:47 +0200

    feat: borrar chats desde la lista (elimina match y mensajes con confirmación)

--- web/sw.js
+++ web/sw.js
@@ -1,4 +1,4 @@
-const VERSION = 'v28';
+const VERSION = 'v29';
 const CACHE = 'sportmatch-' + VERSION;
 const ASSETS = ['/', '/app.html', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png', '/apple-touch-icon.png'];
 
